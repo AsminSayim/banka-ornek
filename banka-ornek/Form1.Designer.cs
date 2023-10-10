f41c09aee1ca90ac908a89a3d10ad30934a2d380
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2023-10-10 16:10:20 +0300

    Add files via upload

---
+++ banka-ornek/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace banka_ornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTutar = new System.Windows.Forms.Label();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.btnYatir = new System.Windows.Forms.Button();
+            this.btnCek = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblBakiye = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTutar
+            // 
+            this.lblTutar.AutoSize = true;
+            this.lblTutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTutar.Location = new System.Drawing.Point(12, 43);
+            this.lblTutar.Name = "lblTutar";
+            this.lblTutar.Size = new System.Drawing.Size(74, 29);
+            this.lblTutar.TabIndex = 0;
+            this.lblTutar.Text = "Tutar";
+            this.lblTutar.Click += new System.EventHandler(this.lblTutar_Click);
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTutar.Location = new System.Drawing.Point(103, 43);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(132, 29);
+            this.txtTutar.TabIndex = 1;
+            // 
+            // btnYatir
+            // 
+            this.btnYatir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYatir.Location = new System.Drawing.Point(61, 91);
+            this.btnYatir.Name = "btnYatir";
+            this.btnYatir.Size = new System.Drawing.Size(94, 38);
+            this.btnYatir.TabIndex = 2;
+            this.btnYatir.Text = "Yatır";
+            this.btnYatir.UseVisualStyleBackColor = true;
+            // 
+            // btnCek
+            // 
+            this.btnCek.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCek.Location = new System.Drawing.Point(161, 91);
+            this.btnCek.Name = "btnCek";
+            this.btnCek.Size = new System.Drawing.Size(98, 38);
+            this.btnCek.TabIndex = 3;
+            this.btnCek.Text = "Çek";
+            this.btnCek.UseVisualStyleBackColor = true;
+            this.btnCek.Click += new System.EventHandler(this.btnCek_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblBakiye);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(329, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 107);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hesap";
+            // 
+            // lblBakiye
+            // 
+            this.lblBakiye.AutoSize = true;
+            this.lblBakiye.Location = new System.Drawing.Point(17, 48);
+            this.lblBakiye.Name = "lblBakiye";
+            this.lblBakiye.Size = new System.Drawing.Size(71, 24);
+            this.lblBakiye.TabIndex = 0;
+            this.lblBakiye.Text = "Bakiye";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 245);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCek);
+            this.Controls.Add(this.btnYatir);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.lblTutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTutar;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.Button btnYatir;
+        private System.Windows.Forms.Button btnCek;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblBakiye;
+    }
+}
+
---
+++ banka-ornek/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace banka_ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double bakiye;
+
+        private void lblTutar_Click(object sender, EventArgs e)
+        {
+            double yatiralacakTutar = Convert.ToDouble(txtTutar.Text);
+            bakiye += yatiralacakTutar;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bakiye = 0;
+            lblBakiye.Text="Bakiye"+bakiye.ToString();
+        }
+
+        private void btnCek_Click(object sender, EventArgs e)
+        {
+            double cekilecekTutar = Convert.ToDouble(txtTutar.Text);
+
+
+            if (cekilecekTutar <= bakiye)
+            {
+                bakiye -= cekilecekTutar;
+            }
+            else
+            {
+                MessageBox.Show("YETERSİZ BAKİYE!!!");
+            }
+
+
+
+
+            lblBakiye.Text = "Bakiye: " + bakiye.ToString() + " ₺";
+        }
+    }
+}
